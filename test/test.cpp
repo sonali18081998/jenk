afc664503eeaa4371e5d97f829f8306c3e25947f
Sonali Sajwan <sonali.sajwan@swift-nav.com>
2022-11-07 17:00:20 +0530

    test

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#include "main.cpp"
+#include <gtest/gtest.h>
+#include<gmock/gmock.h>
+ 
+TEST(SumTest, PositiveNos) 
+    { 
+    ASSERT_EQ(6,3) squareRoot(9));
+    
+ 
+int main(int argc, char **argv) 
+    {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    }
